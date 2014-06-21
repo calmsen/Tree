ca983640bb54744ba3e22a8060a4f26dc5707087
calmsen <icalmsen@gmail.com>
2014-06-21 20:04:55 +1100

    check ajax request

--- Tree.js
+++ Tree.js
@@ -1,45 +1,85 @@
 define("Tree", ["jquery"], function($) {
     var JQUERY_PLUGIN_NAME = "tree";
-    var TREE_NODE_SELECTOR = ".tree-node";
-    var TREE_NODE_INNER_SELECTOR = ".tree-node-inner";
-    var TREE_NODE_IMG_BRANCH_SELECTOR = ".tree-node-img-branche";
-    var TREE_NODE_IMG_LOADING_CLASS = ".tree-node-img-loading";
-    var TREE_NODE_IMG_EMPTY_CLASS = ".tree-node-img-empty";
-    var TREE_NODE_IMG_EMPTY_TEMPLATE = "<span class=\"tree-node-img tree-node-img-empty\"></span>";
     
     function Tree() {
         this.init.apply(this, arguments);
     }
+    
     $.extend(Tree.prototype, {
         defaults: {
             baseUrl: location.protocol + "//" + location.host + "/Tree/"
             , element: $()
+            , helpers: {
+                selectors: {
+                    treeNode: ".tree-node"
+                    , treeNodeInner: ".tree-node-inner"
+                    , treeNodeImgBranch: ".tree-node-img-branche"
+                }
+                , classes: {
+                    treeNodeImgLoading: ".tree-node-img-loading"
+                    , treeNodeImgEmpty: ".tree-node-img-empty"
+                }
+                , templates: {
+                    treeNodeImgEmpty: "<span class=\"tree-node-img tree-node-img-empty\"></span>"
+                }
+            }                
         }
         , init: function(options) {
             $.extend(true, this, this.defaults);            
         }
+        , descendantChilds: function(nodeId) {
+            var $childsElem = this.element.find(this.helpers.selectors.treeNode +"[data-parent-id='" + nodeId + "']");
+            var childs = [];
+            var treeObj = this;
+            $childsElem.each(function() {
+                var $childElem = $(this);
+                var childId = $childElem.data("id");
+                childs.push(childId);
+                if ($childElem.data("type") == "branche") {
+                    childs = childs.concat(treeObj.descendantChilds(childId));
+                }
+            });
+            return childs;
+            
+        }
+        , descendantChildExsists: function(nodeId, childId) {
+            console.log(this.descendantChilds(nodeId));
+            if (this.descendantChilds(nodeId).indexOf(childId) != -1) {
+                return true;
+            }
+            return false;
+        }
+        , targetFolderIsChild: function(nodeId, parentId) {
+            return this.descendantChildExsists(nodeId, parentId);
+        }
         , replaceNodeIfOnlyNeed: function(nodeId, parentId) {
-            var $targetFolderElem = this.element.find(TREE_NODE_SELECTOR +"[data-id='" + parentId + "']");
-            var $nodeElem = this.element.find(TREE_NODE_SELECTOR +"[data-id='" + nodeId + "']");
+            var $targetFolderElem = this.element.find(this.helpers.selectors.treeNode +"[data-id='" + parentId + "']");
+            var $nodeElem = this.element.find(this.helpers.selectors.treeNode +"[data-id='" + nodeId + "']");
+            if ($targetFolderElem.data("type") == "leaf") {
+                return;
+            }
             if ($targetFolderElem.data("id") == $nodeElem.data("parentId")) {
+                return;
+            }
+            if (this.targetFolderIsChild(nodeId, parentId)) {
                 return;
             }
             this.replaceNode(nodeId, parentId);
         }
         
         , replaceNode: function(nodeId, parentId) {
-            var $targetFolderElem = this.element.find(TREE_NODE_SELECTOR +"[data-id='" + parentId + "']");
-            var $brancheImgElem = $targetFolderElem.find(TREE_NODE_IMG_BRANCH_SELECTOR);
-            var $nodeElem = this.element.find(TREE_NODE_SELECTOR +"[data-id='" + nodeId + "']");
+            var $targetFolderElem = this.element.find(this.helpers.selectors.treeNode +"[data-id='" + parentId + "']");
+            var $brancheImgElem = $targetFolderElem.find(this.helpers.selectors.treeNodeImgBranche);
+            var $nodeElem = this.element.find(this.helpers.selectors.treeNode +"[data-id='" + nodeId + "']");
             
             this.replaceNodeElem(nodeId, parentId);
-            
+            var treeObj = this;
             $.ajax({
                 url: this.baseUrl + "replaceNode.php"
                 , data: {nodeId: nodeId, parentId: parentId}
                 , type: "POST"
                 , success: function(data) {
-                    $brancheImgElem.removeClass(TREE_NODE_IMG_LOADING_CLASS);
+                    $brancheImgElem.removeClass(treeObj.helpers.classes.treeNodeImgLoading);
                     $nodeElem.show()
                 }
                 , error: function(xhr, status) {
@@ -48,24 +88,24 @@ define("Tree", ["jquery"], function($) {
             });
         }
         , replaceNodeElem: function(nodeId, parentId) {
-            var $targetFolderElem = this.element.find(TREE_NODE_SELECTOR +"[data-id='" + parentId + "']");
-            var $brancheImgElem = $targetFolderElem.find(TREE_NODE_IMG_BRANCH_SELECTOR);
-            var $nodeElem = this.element.find(TREE_NODE_SELECTOR +"[data-id='" + nodeId + "']");
+            var $targetFolderElem = this.element.find(this.helpers.selectors.treeNode +"[data-id='" + parentId + "']");
+            var $brancheImgElem = $targetFolderElem.find(this.helpers.selectors.treeNodeImgBranche);
+            var $nodeElem = this.element.find(this.helpers.selectors.treeNode +"[data-id='" + nodeId + "']");
             
             $nodeElem.hide()
                     .insertAfter($targetFolderElem).data("parentId", $targetFolderElem.data("id"))
-                    .find(TREE_NODE_IMG_EMPTY_CLASS).remove();
+                    .find(this.helpers.classes.treeNodeImgEmpty).remove();
             
             var nodeLevel = $targetFolderElem.data("level") + 1;
-            var $nodeInnerElem = $nodeElem.find(TREE_NODE_INNER_SELECTOR);
+            var $nodeInnerElem = $nodeElem.find(this.helpers.selectors.treeNodeInner);
             for (var i = 1; i < nodeLevel; i++) {
-                $nodeInnerElem.prepend(TREE_NODE_IMG_EMPTY_TEMPLATE);
+                $nodeInnerElem.prepend(this.helpers.templates.treeNodeImgEmpty);
             }
             $nodeElem.data("level", nodeLevel);
             
             var nodeType = $nodeElem.data("type");
             if (nodeType == "leaf") {
-                $nodeElem.prepend(TREE_NODE_IMG_EMPTY_TEMPLATE);
+                $nodeElem.prepend(this.helpers.templates.treeNodeImgEmpty);
             }
         }
     });
