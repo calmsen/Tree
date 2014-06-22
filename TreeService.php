8393b5cb265a1e84b2bd52ccbae4770e2ed33271
calmsen <icalmsen@gmail.com>
2014-06-22 12:55:56 +1100

    edit TreeServie

--- TreeService.php
+++ TreeService.php
@@ -11,10 +11,17 @@ class TreeService {
         $nodes = $this->repository->getTree($parentId);
         foreach($nodes as $node) {
             $childs = [];
-            if ($node->type == "branch" && true/** in_array($node->id, $expandedBranches)*/) {
+            $expanded = true; // нужно ли раскрывать папку
+            if (count($expandedBranches) > 0) {
+                $expanded = false;
+                if (in_array($node->id, $expandedBranches)) {
+                    $expanded = true;
+                }
+            }
+            if ($node->type == "branch" && $expanded) {
                 $childs = $this->getTree($expandedBranches, $node->id, $level + 1);
             }
-            $tree[] = new TreeDomain($node->id, $node->name, $node->type, $node->parentId, true/** in_array($node->id, $expandedBranches)*/, $childs, $level);
+            $tree[] = new TreeDomain($node->id, $node->name, $node->type, $node->parentId, $expanded, $childs, $level);
         }
         return $tree;
     }
